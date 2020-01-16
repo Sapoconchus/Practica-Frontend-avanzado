8701e63f70a7d2a76a4fb28bcc3f7634a68246ca
Mario Vidal <mavidalma@gmail.com>
2020-01-16 12:59:43 +0100

    retoques maquetación, responsive pics y eventos onclick

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,26 @@
-import {getBeers} from "./api.js";
+//import {getBeers} from "./api.js";
 
 
 const input = document.querySelector(".input.search");
 const form = document.getElementById("search-form");
+const button = document.querySelector("button");
+const searchGlass = document.querySelector(".navbar-icon");
+const closeButton = document.querySelector(".close")
+const navLogo = document.getElementById("nav-logo");
 
-form.addEventListener("onsubmit", evt => {
+searchGlass.addEventListener("click", evt =>{
+	form.classList.toggle("no-display");
+	searchGlass.classList.toggle("no-display");
+});
+
+closeButton.addEventListener("click", evt =>{
+	form.classList.toggle("no-display");
+	searchGlass.classList.toggle("no-display");
+});
+
+
+
+form.addEventListener("submit", evt => {
  evt.preventDefault();
 
      console.log("hola")
